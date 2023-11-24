63cacdb8963e6494e0e5eef1c8a8917fb031156b
Parthiv <parthivkumarkamleshbhai.patel@dcmiail.ca>
2023-11-24 15:23:18 -0500

    Commit 6
    I have made all the necessary changes and my file is ready to get published.

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -42,7 +42,7 @@ function result() {
   if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
-    
+
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
   }
